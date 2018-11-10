61845afb24f9b0bad0a1dbd8f846574dfa9d136d
PranavShekhar13 <43288779+PranavShekhar13@users.noreply.github.com>
2018-11-10 11:01:24 +0530

    Add files via upload

---
+++ armstrong_rec.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>               /*ARMSTRONG OF A NUMBER USING RECURSIVE FUNCTION */
+int armstrong(int n);
+int main()
+{
+int n,result;
+int b=n;
+printf("Enter the number which needs to be checked for Armstrong");
+scanf("%d",&n);
+result=armstrong(n);
+if(result==b)
+printf("Armstrong Number");
+else
+printf("Not an Armstrong Number");
+return 0;
+}
+int armstrong(int n)
+{
+int result;
+if(n==0)
+result=0;
+else
+result=pow(n%10,3)+armstrong(n/10);
+return result;
+}
