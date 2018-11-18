1dd9d76cf3f99c888b58b6f97feb31fb9cb7edf7
PranavShekhar13 <43288779+PranavShekhar13@users.noreply.github.com>
2018-11-18 18:49:21 +0530

    Add files via upload

---
+++ stringlen.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int i,len=0;
+scanf("%[^\n]",a);
+for(int i=0;a[i]!='\0';i++)
+{
+len=len+1;
+}
+printf("Length of the String=%d",len);
+}
