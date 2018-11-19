5417f9525195e881b2dfc03b310f00ac980ed5e0
PranavShekhar13 <43288779+PranavShekhar13@users.noreply.github.com>
2018-11-19 06:57:59 +0530

    Update hcf_recur.c

--- hcf_recur.c
+++ hcf_recur.c
@@ -1,5 +1,5 @@
-#include<stdio.h>  /*8. TO FIND PRODUCT OF TWO NUMBERS USING RECURSIVE FUNCTION*/ 
-int product(int a,int b);
+#include<stdio.h>  /*8. TO FIND HCF OF TWO NUMBERS USING RECURSIVE FUNCTION*/ 
+int HCF(int a,int b);
 int main()
 {
 int a,b,res=0;
@@ -7,24 +7,24 @@ printf("Enter the first number");
 scanf("%d",&a);
 printf("\nEnter the second number");
 scanf("%d",&b);
-res=product(a,b);
-printf("The product of %d and %d = %d",a,b,res);
+res=HCF(a,b);
+printf("The HCF of %d and %d = %d",a,b,res);
 return 0;
 }
 
-int product(int a,int b)
+int HCF(int a,int b)
 {
-if(a<b)
+if(a>b)
 {
-return product(b,a);
+HCF(a-b,b);
 }
-else if(b!=0)
+else if(a<b)
 {
-return (a+product(a,b-1));
+HCF(a.b-a);
 }
 else
 { 
-return 0;
+return x;
 }
 
 }
